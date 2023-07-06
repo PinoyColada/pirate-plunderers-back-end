396d611a5f45bae200b13ff7e81d130f6a2f149f
pinoycolada <pinoydol.colada@gmail.com>
2023-07-06 02:02:47 -0400

    added cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import pandas
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 #assign constants to json and csv file
 FILEPATH = 'top10.csv'
